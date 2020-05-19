25329efeec3cd40aa40c09a304271ab8b64e2a67
Sid <evaska.sierra@gmail.com>
2020-05-19 18:36:05 -0500

    init

---
+++ FAQ_Bot/FAQ_Bot/FAQ_Bot_Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FAQ_Bot
+{
+    class FAQ_Bot_Main
+    {
+        static void Main(string[] args)
+        
+            => new FAQ_Bot_Main().MainAsync().GetAwaiter().GetResult();
+
+            public async Task MainAsync()
+            {
+            }
+        }
+    }
+}
