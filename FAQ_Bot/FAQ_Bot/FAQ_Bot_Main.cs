96f2d962b0fdc0bfb39b77b621be7cd78d4fb04d
Sid <evaska.sierra@gmail.com>
2020-05-19 21:03:03 -0500

    added link to journal

--- FAQ_Bot/FAQ_Bot/FAQ_Bot_Main.cs
+++ FAQ_Bot/FAQ_Bot/FAQ_Bot_Main.cs
@@ -12,7 +12,7 @@ namespace FAQ_Bot
         
             => new FAQ_Bot_Main().MainAsync().GetAwaiter().GetResult();
 
-        List<String> faqList = new List<String>();
+            List<String> faqList = new List<String>();
         
 
         private DiscordSocketClient _client;
@@ -20,12 +20,15 @@ namespace FAQ_Bot
             public async Task MainAsync()
             {
 
+            //FAQ List
+            faqList.Add("Thank you for using FAQBot! Please review this journal:https://www.deviantart.com/artspacious/journal/Feature-Requests-and-FAQ-839151383 . Below you will find questions that have already been asked in this server. ");
             faqList.Add("1.) Is the site ready/open yet? ");
             faqList.Add("1A: ArtSpacious is not open yet. The project was started on April 20th, 2020. We do not yet have an estimated date for when the site will be ready.");
             faqList.Add("2.) Are there any screenshots of the site? ");
             faqList.Add("2A: There are currently no screenshots of the site yet. We have not yet decided on a UI or theme or anything. The main focus currently is functionality and security.");
             faqList.Add("3.) How many themes will there be? Will the theme be easy to read? Do you have an example or mockups of the UI? ");
             faqList.Add("3A: As ArtSpacious is still in the early development stages, we do not have a definitive answer for this yet. We will however be ensuring that our themes and site are accesible to as many people as possible.");
+            
             _client = new DiscordSocketClient();
 
             _client.Log += Log;
